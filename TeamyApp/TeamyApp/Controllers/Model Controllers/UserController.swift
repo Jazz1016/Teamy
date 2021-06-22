9cdd86ece23f0e03cfc77196f8d29b8b94ae0148
James Lea <jameslea@Jamess-MBP.localdomain>
2021-06-22 14:36:01 -0600

    commiting changes to pull

--- TeamyApp/TeamyApp/Controllers/Model Controllers/UserController.swift
+++ TeamyApp/TeamyApp/Controllers/Model Controllers/UserController.swift
@@ -13,6 +13,41 @@ class UserController {
     
     var user: User?
     
+    let db = Firestore.firestore()
     
-    
-}
+    func inviteUserToTeam(userEmail: String, teamId: String){
+        var userQueried = db.collection("users").whereField("email", isEqualTo: userEmail)
+        userQueried.getDocuments { snap, error in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+            }
+            
+            guard let snap = snap else {return}
+            
+            if snap.count == 1 {
+                let userData = snap.documents[0].data()
+                
+                let email = userData["email"] as? String
+                let firstName = userData["firstName"] as? String
+                let lastName = userData["lastName"] as? String
+                var invites = userData["invites"] as? Array<String> ?? []
+                let teams = userData["teams"] as? Array<String> ?? []
+                let userId =  userData["userId"] as? String
+                
+                invites.append(teamId)
+                
+                
+                self.db.collection("users").document(userId!).setData([
+                    
+                    "email" : email,
+                    "firstName" : firstName,
+                    "lastName" : lastName,
+                    "invites" : invites,
+                    "teams" : teams,
+                    "userId" : userId
+                ])
+                
+            }
+        }
+    }
+}//End of class
--- TeamyApp/TeamyApp/Controllers/View Controllers/HomeViewController.swift
+++ TeamyApp/TeamyApp/Controllers/View Controllers/HomeViewController.swift
@@ -19,7 +19,6 @@ class HomeViewController: UIViewController {
         } catch {
             print("error")
         }
-        
         if Auth.auth().currentUser == nil {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let VC = storyboard.instantiateViewController(identifier: "AuthVC")
