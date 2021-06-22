e7c1f8cb3ce88e0410768e38e8fa6d6dee343de5
Ethan Scott <Ethan@Ethans-lit-Mac-mini.local>
2021-06-22 14:18:53 -0600

    created login and sign up functionality. Currently does not redirect user to home view controller.

--- TeamyApp/TeamyApp/Controllers/View Controllers/AuthViewControllers/SignUpViewController.swift
+++ TeamyApp/TeamyApp/Controllers/View Controllers/AuthViewControllers/SignUpViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
     // MARK: - Outlets
@@ -22,8 +23,41 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
-        
+        SignUp()
     }
+    
+    func SignUp() {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty,
+              let confirmPassword = passwordConfirmTextField.text, !password.isEmpty,
+              let firstName = firstNameTextField.text, !firstName.isEmpty,
+              let lastName = lastNameTextField.text, !lastName.isEmpty else {return}
+        
+        if confirmPassword == password {
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                if let error = error {
+                    print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                }
+                if result != nil {
+                    print("Successfully created account")
+//                    self.transitionToHome()
+                }
+            }
+        } else {
+            //present alert saying passwords don't match.
+            let alert = UIAlertController(title: "Sign Up Error", message: "Passwords are not identical", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func transitionToHome() {
+            let homeViewController = storyboard?.instantiateViewController(identifier: "HomeVC")
+            
+            view.window?.rootViewController = homeViewController
+            view.window?.makeKeyAndVisible()
+        }
     
 
     /*
