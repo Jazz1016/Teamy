18fd78f712d5a70c346af8f25489d4723f2eb042
James Lea <jameslea@Jamess-MacBook-Pro.local>
2021-06-29 18:07:20 -0600

    added rostersVC and needed pieces

---
+++ TeamyApp/TeamyApp/Views/Custom Views/Custom Cells/PlayerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerTableViewCell.swift
+//  TeamyApp
+//
+//  Created by James Lea on 6/29/21.
+//
+
+import UIKit
+
+class PlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
