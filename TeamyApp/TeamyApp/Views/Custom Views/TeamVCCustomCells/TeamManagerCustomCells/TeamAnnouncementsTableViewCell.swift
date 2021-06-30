0a66c4a53e71d3de4e924c97fbcc78d3980e7ed2
Anthony Byrd <scrill21@gmail.com>
2021-06-29 21:18:50 -0400

    Added Manage Team VC and custom cells

---
+++ TeamyApp/TeamyApp/Views/Custom Views/TeamVCCustomCells/TeamManagerCustomCells/TeamAnnouncementsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TeamAnnouncementsTableViewCell.swift
+//  TeamyApp
+//
+//  Created by anthony byrd on 6/29/21.
+//
+
+import UIKit
+
+class TeamAnnouncementsTableViewCell: UITableViewCell {
+    
+    static let identifier = "TeamAnnouncementsTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
