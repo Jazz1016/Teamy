54f359b9af0b254b0abd5000cada41b9beaefab6
Anthony Byrd <scrill21@gmail.com>
2021-07-06 11:26:25 -0400

    Added the editTeamContactsVC

--- TeamyApp/TeamyApp/Controllers/View Controllers/AdminViewControllers/EditContactsViewController.swift
+++ TeamyApp/TeamyApp/Controllers/View Controllers/AdminViewControllers/EditContactsViewController.swift
@@ -21,7 +21,39 @@ class EditContactsViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func addContactButtonTapped(_ sender: Any) {
+        let createNewContactAlert = UIAlertController(title: "Create New Contact", message: "Please fill out all fields", preferredStyle: .alert)
         
+        createNewContactAlert.addTextField { textfield in
+            textfield.placeholder = "Contact name"
+        }
+        
+        createNewContactAlert.addTextField { textfield in
+            textfield.placeholder = "Contact method"
+        }
+        
+        createNewContactAlert.addTextField { textfield in
+            textfield.placeholder = "Enter method"
+        }
+        
+        let addContactAction = UIAlertAction(title: "Create", style: .default) { action in
+            
+            guard let contactName = createNewContactAlert.textFields![0].text,
+                  let contactType = createNewContactAlert.textFields![1].text,
+                  let contactInfo = createNewContactAlert.textFields![2].text,
+                  let teamId = EventController.shared.team else { return }
+            
+            let newContact = Contact(contactName: contactName, contactType: contactType, contactInfo: contactInfo)
+            
+            ContactController.shared.createContact(contact: newContact, teamId: teamId.teamId)
+            
+            self.contactsTableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        createNewContactAlert.addAction(cancelAction)
+        createNewContactAlert.addAction(addContactAction)
+        present(createNewContactAlert, animated: true, completion: nil)
     }
     
 }
