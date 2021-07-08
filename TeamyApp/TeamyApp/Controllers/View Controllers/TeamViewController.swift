39b7891833dc988a344642bedd3e6a4fdea60642
James Lea <jameslea@Jamess-MBP.localdomain>
2021-07-08 15:26:42 -0600

    blocking functionality fully built out

--- TeamyApp/TeamyApp/Controllers/View Controllers/TeamViewController.swift
+++ TeamyApp/TeamyApp/Controllers/View Controllers/TeamViewController.swift
@@ -44,22 +44,6 @@ class TeamViewController: UIViewController {
             }
             fetchDetails()
             fetchEvents()
-            guard let team = team else {return}
-            UserController.shared.fetchUsers(userIds: team.admins, access: "admin") { result in
-                if !result {
-                    print("Error Fetching admins")
-                }
-            }
-            UserController.shared.fetchUsers(userIds: team.members, access: "member") { result in
-                if !result {
-                    print("Error Fetching members")
-                }
-            }
-            UserController.shared.fetchUsers(userIds: team.blocked, access: "blocked") { result in
-                if !result {
-                    print("Error Fetching blocked users")
-                }
-            }
         }
     }
     
