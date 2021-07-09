76e26d7bc14aec6402e8ad32dad1f30d4f955923
Ethan Scott <Ethan@Ethans-lit-Mac-mini.local>
2021-07-08 21:12:55 -0600

    Added scroll views to Create Event and Create Team VCs. App is pretty much done

--- TeamyApp/TeamyApp/Controllers/View Controllers/CreateEventViewController.swift
+++ TeamyApp/TeamyApp/Controllers/View Controllers/CreateEventViewController.swift
@@ -122,12 +122,13 @@ class CreateEventViewController: UIViewController {
         }
         hideKeyboardWhenTappedAround()
         navigationController?.navigationBar.prefersLargeTitles = false
+        
     }
-    
+
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height / 2
+                self.view.frame.origin.y -= keyboardSize.height / 4
             }
         }
     }
